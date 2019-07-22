d3778f99de4bde8408ac865ca9616905dc069645
kapxapot <kapxapot@gmail.com>
2019-07-22 23:25:18 +0300

    Countdown fix

--- src/Services/SidebarPartsProviderService.php
+++ src/Services/SidebarPartsProviderService.php
@@ -27,48 +27,44 @@ class SidebarPartsProviderService extends Contained
     {
         $result = null;
         
-		switch ($part) {
-			case 'news':
-				$limit = $this->getSettings('sidebar.latest_news_limit');
-				$exceptNewsId = $settings['news_id'] ?? null;
-				
-				$result = $this->newsAggregatorService->getLatest($game, $limit, $exceptNewsId);
-				break;
+        switch ($part) {
+            case 'news':
+                $limit = $this->getSettings('sidebar.latest_news_limit');
+                $exceptNewsId = $settings['news_id'] ?? null;
+                
+                $result = $this->newsAggregatorService->getLatest($game, $limit, $exceptNewsId);
+                break;
 
-			case 'articles':
-				$limit = $this->getSettings('sidebar.article_limit');
-				$exceptArticleId = $settings['article_id'] ?? null;
-				
-				$result = Article::getLatest($game, $limit, $exceptArticleId)->all();
-				break;
-			
-			case 'stream':
-				$result = [
-				    'stream' => $this->streamService->topOnline($game),
-				    'total_online' => $this->streamService->totalOnlineStr(),
-				];
-				break;
-			
-			case 'gallery':
-				$limit = $this->getSettings('sidebar.latest_gallery_pictures_limit');
-				$result = [ 'pictures' => GalleryPicture::getLatestByGame($game, $limit)->all() ];
-				break;
-			
-			case 'events':
-				$days = $this->getSettings('sidebar.future_events_days');
-				$result = Event::getCurrent($game, $days)->all();
-				break;
-			
-			case 'countdown':
-			    $event = Event::getImportant()->one();
-
-			    if ($event !== null) {
-			        $result = [ 'event' => $event ];
-			    }
-			    
-			    break;
-		}
-		
-		return $result;
+            case 'articles':
+                $limit = $this->getSettings('sidebar.article_limit');
+                $exceptArticleId = $settings['article_id'] ?? null;
+                
+                $result = Article::getLatest($game, $limit, $exceptArticleId)->all();
+                break;
+            
+            case 'stream':
+                $result = [
+                    'stream' => $this->streamService->topOnline($game),
+                    'total_online' => $this->streamService->totalOnlineStr(),
+                ];
+                break;
+            
+            case 'gallery':
+                $limit = $this->getSettings('sidebar.latest_gallery_pictures_limit');
+                $result = [ 'pictures' => GalleryPicture::getLatestByGame($game, $limit)->all() ];
+                break;
+            
+            case 'events':
+                $days = $this->getSettings('sidebar.future_events_days');
+                $result = Event::getCurrent($game, $days)->all();
+                break;
+            
+            case 'countdown':
+                $event = Event::getImportant()->one();
+                $result = [ 'event' => $event ];
+                break;
+        }
+        
+        return $result;
     }
 }
