6a692a874098f1793b1be8250222fb2ecdabc0bc
kapxapot <kapxapot@gmail.com>
2019-11-04 16:21:17 +0300

    Videos added to news feed

--- src/Services/NewsAggregatorService.php
+++ src/Services/NewsAggregatorService.php
@@ -2,16 +2,16 @@
 
 namespace App\Services;
 
-use Plasticode\Collection;
-use Plasticode\Util\Arrays;
-use Plasticode\Util\Date;
-use Plasticode\Util\Sort;
-
 use App\Models\Article;
 use App\Models\Event;
 use App\Models\ForumTopic;
 use App\Models\News;
 use App\Models\NewsYear;
+use App\Models\Video;
+use Plasticode\Collection;
+use Plasticode\Util\Arrays;
+use Plasticode\Util\Date;
+use Plasticode\Util\Sort;
 
 class NewsAggregatorService
 {
@@ -20,6 +20,7 @@ class NewsAggregatorService
         Event::class,
         ForumTopic::class,
         News::class,
+        Video::class
     ];
     
     private $strictSources = [
@@ -66,7 +67,7 @@ class NewsAggregatorService
             return $s::getNewsByTag($tag)->all();
         });
 
-		return $this->sort($all);
+        return $this->sort($all);
     }
 
     public function getCount($game = null, bool $strict = false) : int
@@ -78,22 +79,22 @@ class NewsAggregatorService
         return array_sum($counts);
     }
     
-	public function getLatest($game, $limit, $exceptNewsId = null, bool $strict = true) : Collection
-	{
-		return $this->getPage($game, 1, $limit, $exceptNewsId, $strict);
-	}
-	
-	private function dump($data)
-	{
+    public function getLatest($game, $limit, $exceptNewsId = null, bool $strict = true) : Collection
+    {
+        return $this->getPage($game, 1, $limit, $exceptNewsId, $strict);
+    }
+    
+    private function dump($data)
+    {
         var_dump($data->map(function ($item) {
             return [
                 $item->entityAlias(), $item->id, $item->publishedAt
             ];
         }));
-	}
+    }
 
-	public function getPage($game = null, int $page = 1, int $pageSize = 7, $exceptNewsId = null, bool $strict = false) : Collection
-	{
+    public function getPage($game = null, int $page = 1, int $pageSize = 7, $exceptNewsId = null, bool $strict = false) : Collection
+    {
         if ($page < 1) {
             $page = 1;
         }
@@ -102,10 +103,10 @@ class NewsAggregatorService
             throw new \InvalidArgumentException('$pageSize must be a positive integer.');
         }
 
-		$offset = ($page - 1) * $pageSize;
-		
-		// optimization for faster load
-		$loadLimit = $offset + $pageSize;
+        $offset = ($page - 1) * $pageSize;
+        
+        // optimization for faster load
+        $loadLimit = $offset + $pageSize;
 
         $all = $this->collect($strict, function ($s) use ($game, $exceptNewsId, $loadLimit) {
             return $s::getLatestNews($game, $exceptNewsId)
@@ -116,131 +117,131 @@ class NewsAggregatorService
         $all = $this->sort($all);
 
         return $all->slice($offset, $pageSize);
-	}
-	
-	/**
-	 * Looks for News or ForumTopic with the provided id
-	 */
-	public function getNews($newsId)
-	{
-	    return News::getProtected()->find($newsId)
-	        ??
-	        ForumTopic::getNews($newsId);
-	}
-	
-	public function getPrev($news, $strict = true)
-	{
-	    $date = $news->publishedAt;
-	    $game = $news->game
-	        ? $news->game->root
-	        : null;
+    }
+    
+    /**
+     * Looks for News or ForumTopic with the provided id
+     */
+    public function getNews($newsId)
+    {
+        return News::getProtected()->find($newsId)
+            ??
+            ForumTopic::getNews($newsId);
+    }
+    
+    public function getPrev($news, $strict = true)
+    {
+        $date = $news->publishedAt;
+        $game = $news->game
+            ? $news->game->root
+            : null;
 
-	    $allPrev = $this->collect($strict, function ($s) use ($game, $date) {
-	        return $s::getNewsBefore($game, $date)
-	            ->limit(1)
-	            ->all();
-	    });
-	    
-	    $allPrev = $this->sort($allPrev);
-	    
-	    return $allPrev->first();
-	}
-	
-	public function getNext($news, $strict = true)
-	{
-	    $date = $news->publishedAt;
-	    $game = $news->game
-	        ? $news->game->root
-	        : null;
-	    
-	    $allNext = $this->collect($strict, function ($s) use ($game, $date) {
-	        return $s::getNewsAfter($game, $date)
-	            ->limit(1)
-	            ->all();
-	    });
+        $allPrev = $this->collect($strict, function ($s) use ($game, $date) {
+            return $s::getNewsBefore($game, $date)
+                ->limit(1)
+                ->all();
+        });
+        
+        $allPrev = $this->sort($allPrev);
+        
+        return $allPrev->first();
+    }
+    
+    public function getNext($news, $strict = true)
+    {
+        $date = $news->publishedAt;
+        $game = $news->game
+            ? $news->game->root
+            : null;
+        
+        $allNext = $this->collect($strict, function ($s) use ($game, $date) {
+            return $s::getNewsAfter($game, $date)
+                ->limit(1)
+                ->all();
+        });
 
-	    $allNext = $this->sortReverse($allNext);
-	    
-	    return $allNext->first();
-	}
-	
-	private function getAllRaw(bool $strict = false) : Collection
-	{
+        $allNext = $this->sortReverse($allNext);
+        
+        return $allNext->first();
+    }
+    
+    private function getAllRaw(bool $strict = false) : Collection
+    {
         return $this->collect($strict, function ($s) {
             return $s::getLatestNews()->all();
         });
-	}
-	
-	public function getTop($limit, bool $strict = false) : Collection
-	{
-	    return $this->getPage(null, 1, $limit, null, $strict);
-	}
+    }
+    
+    public function getTop($limit, bool $strict = false) : Collection
+    {
+        return $this->getPage(null, 1, $limit, null, $strict);
+    }
 
     /**
      * Descending.
      */
-	public function getYears(bool $strict = true) : Collection
-	{
-	    $byYear = self::getAllRaw($strict)
-	        ->group(function ($item) {
-	            return Date::year($item->publishedAtIso());
-	        });
+    public function getYears(bool $strict = true) : Collection
+    {
+        $byYear = self::getAllRaw($strict)
+            ->group(function ($item) {
+                return Date::year($item->publishedAtIso());
+            });
 
-		$years = array_keys($byYear);
-		
-		return Collection::make($years)
-		    ->map(function ($y) {
-		        return new NewsYear($y);
-		    })
-		    ->desc('year');
-	}
-	
-	public function getPrevYear($year, bool $strict = true)
-	{
-	    return $this->getYears($strict)
-	        ->where(function ($y) use ($year) {
-	            return $y->year < $year;
-	        })
-	        ->desc('year')
-	        ->first();
-	}
-	
-	public function getNextYear($year, bool $strict = true)
-	{
-	    return $this->getYears($strict)
-	        ->where(function ($y) use ($year) {
-	            return $y->year > $year;
-	        })
-	        ->asc('year')
-	        ->first();
-	}
-	
-	public function getByYear($year, bool $strict = true) : array
-	{
-	    $byYear = $this->collect($strict, function ($s) use ($year) {
+        $years = array_keys($byYear);
+        
+        return Collection::make($years)
+            ->map(function ($y) {
+                return new NewsYear($y);
+            })
+            ->desc('year');
+    }
+    
+    public function getPrevYear($year, bool $strict = true)
+    {
+        return $this->getYears($strict)
+            ->where(function ($y) use ($year) {
+                return $y->year < $year;
+            })
+            ->desc('year')
+            ->first();
+    }
+    
+    public function getNextYear($year, bool $strict = true)
+    {
+        return $this->getYears($strict)
+            ->where(function ($y) use ($year) {
+                return $y->year > $year;
+            })
+            ->asc('year')
+            ->first();
+    }
+    
+    public function getByYear($year, bool $strict = true) : array
+    {
+        $byYear = $this->collect($strict, function ($s) use ($year) {
             return $s::getNewsByYear($year)->all();
         });
 
-		$sorted = $this->sort($byYear);
-		
-		$monthly = [];
-		
-		foreach ($sorted as $s) {
-			$month = Date::month($s->publishedAtIso());
-			
-			if (!array_key_exists($month, $monthly)) {
-				$monthly[$month] = [
-					'label' => Date::SHORT_MONTHS[$month],
-					'full_label' => Date::MONTHS[$month],
-					'news' => [],
-				];
-			}
-			
-			$monthly[$month]['news'][] = $s;
-		}
+        $sorted = $this->sort($byYear);
+        
+        $monthly = [];
+        
+        foreach ($sorted as $s) {
+            $month = Date::month($s->publishedAtIso());
+            
+            if (!array_key_exists($month, $monthly)) {
+                $monthly[$month] = [
+                    'label' => Date::SHORT_MONTHS[$month],
+                    'full_label' => Date::MONTHS[$month],
+                    'news' => [],
+                ];
+            }
+            
+            $monthly[$month]['news'][] = $s;
+        }
 
-		ksort($monthly);
+        ksort($monthly);
 
-		return $monthly;
-	}
+        return $monthly;
+    }
 }
